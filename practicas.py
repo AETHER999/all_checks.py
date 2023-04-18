82221ea645d25fd39effdf25ac231ceae6251eee
sebastian <sebastianduke08@yahoo.com>
2023-04-18 15:46:27 +0200

    periods deleted at the end of 4 paragraph

--- practicas.py
+++ practicas.py
@@ -2,8 +2,8 @@
 import os
 import sys
 def check_reboot():
-    """Returns True if the computer has a pending reboot...
-    return os.path.exist("/run/reboot-require...")
+    """Returns True if the computer has a pending reboot
+    return os.path.exist("/run/reboot-require.")
 def main()
     if check_reboot():
         print("pending reboot.")
