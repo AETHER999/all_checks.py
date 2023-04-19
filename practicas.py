6eabef776dac6f0bc37d611fdce19b3327c26fbd
sebastian <sebastianduke08@yahoo.com>
2023-04-19 23:30:59 +0200

    add check_reboot function

--- practicas.py
+++ practicas.py
@@ -2,13 +2,15 @@
 import os
 import sys
 
+def check_reboot():
+    """Returns true if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
 
 def main():
     if check_reboot():
         print("pending reboot.")
         sys.exit(1)
-    print("everything ok.")
+    print("everythin ok.")
     sys.exit(0)
 
-
-main()
+    main()
