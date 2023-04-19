5e94e1181d239cabf3feded2c47368466caea17d
sebastian <sebastianduke08@yahoo.com>
2023-04-19 23:16:43 +0200

    add pychache file

--- practicas.py
+++ practicas.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 import os
 import sys
-def check_reboot():
-    """Returns True if the computer has a pending reboot"""
-    return os.path.exist("/run/reboot-require.")
-def main()
+
+
+def main():
     if check_reboot():
         print("pending reboot.")
         sys.exit(1)
     print("everything ok.")
     sys.exit(0)
-    
+
 
 main()
