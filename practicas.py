d251ea40f610dfee274fb1040a573fb6ff763779
sebastian <sebastianduke08@yahoo.com>
2023-04-19 23:43:04 +0200

    Se elimino la funcion check_reboot

--- practicas.py
+++ practicas.py
@@ -2,9 +2,7 @@
 import os
 import sys
 
-def check_reboot():
-    """Returns true if the computer has a pending reboot."""
-    return os.path.exists("/run/reboot-required")
+
 
 def main():
     if check_reboot():
