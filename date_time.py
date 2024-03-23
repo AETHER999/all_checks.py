9527b8fc4d21803dbe16e33e682999cbdd2a8a32
c-bas <47993782+AETHER999@users.noreply.github.com>
2024-03-23 19:04:42 +0100

    Delete date_time.py

--- date_time.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-# Get the current date and time
-now = datetime.datetime.now()
-
-# Print the current date and time
-print('Current date and time:')
-print(now.strftime('%Y-%m-%d %H:%M:%S'))
